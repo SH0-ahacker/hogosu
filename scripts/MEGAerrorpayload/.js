23a6e21e8c9e9f6f9c679e90f2f99fd93f7289ae
lagalapizza_1011 <52937131+SH0-ahacker@users.noreply.github.com>
2019-12-01 20:26:12 -0300

    Create .js

---
+++ scripts/MEGAerrorpayload/.js
@@ -0,0 +1,31 @@
+// attention!, this is a maware file!, don't run is in your PC!, this is a test!
+function MEGAerrorpayload () {
+console.log("ha ha ha ")
+console.info("kkkkkkkkkkkk");
+console.group("lolololololol");
+console.warn("hahahahahaha, lol!");
+console.trace();
+console.info("kkkkkkkkkkkk");
+console.warn("hahahahahaha, lol!");
+console.log("ha ha h4")
+console.info("kkkkkkkkkkkk");
+console.info("kkkkkkkkkkkk");
+console.group("lolololololol");
+console.log("h77a ha ")
+console.log("a ha ")
+console.trace();
+console.group("lolololololol");
+console.log("ha ha h ")
+console.info("kkkkkkkkkkkk");
+console.log("ha      ha ha ")
+console.log("ha ha ha                                 ")
+console.info("kkkkkkkkkkkk");
+console.error("hahahahahah, l'am a monster!!!!!!!!")
+console.warn("hahahahahaha, lol!");
+console.info("kkkkkkkkkkkk");
+MEGAerrorpayloadSTART ()
+}
+function MEGAerrorpayloadSTART () {
+MEGAerrorpayload ()
+}
+MEGAerrorpayloadSTART ()
