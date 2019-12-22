c588c9b97e65789d31365b40d02666faa61afa9d
lagalapizza_1011 <52937131+SH0-ahacker@users.noreply.github.com>
2019-12-22 16:58:20 -0300

    Create .js

---
+++ scripts/open_("meme name")_MEME ()/open_eeeaaaooo_MEME ()/.js
@@ -0,0 +1,12 @@
+function openInNewTab(href) {
+  Object.assign(document.createElement('a'), {
+    target: '_blank',
+    href,
+  }).click();
+}
+
+open_eeeaaaooo_MEME ()
+
+function open_eeeaaaooo_MEME () {
+openInNewTab("https://www.youtube.com/watch?v=BzV4BkxtyYs")
+}
