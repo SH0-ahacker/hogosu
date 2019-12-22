fee2ed7a7a6fff4191bd627aff7a0fbacea252af
lagalapizza_1011 <52937131+SH0-ahacker@users.noreply.github.com>
2019-12-22 17:06:25 -0300

    Create .js

---
+++ scripts/open_("meme name")_MEME ()/open_sadviolinairhorn_MEME ()/.js
@@ -0,0 +1,12 @@
+function openInNewTab(href) {
+  Object.assign(document.createElement('a'), {
+    target: '_blank',
+    href,
+  }).click();
+}
+
+open_sadviolinairhorn_MEME ()
+
+function open_sadviolinairhorn_MEME () {
+openInNewTab("https://www.youtube.com/watch?v=BfyXK4KwyYA&list=PLPbYmF3GfbKhKZKdu551Lq776n9-Qsnlc&index=11")
+}
