a66bb4c9386fb81c856473623f8f90a728282262
lagalapizza_1011 <52937131+SH0-ahacker@users.noreply.github.com>
2019-12-22 17:01:02 -0300

    Create .js

---
+++ scripts/open_("meme name")_MEME ()/open_momgetthecamera_MEME ()/.js
@@ -0,0 +1,12 @@
+function openInNewTab(href) {
+  Object.assign(document.createElement('a'), {
+    target: '_blank',
+    href,
+  }).click();
+}
+
+open_momgetthecamera_MEME ()
+
+function open_momgetthecamera_MEME () {
+openInNewTab("https://www.youtube.com/watch?v=8fsH1Rp_5bk")
+}
