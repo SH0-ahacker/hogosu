fa7538e3589dd4ce4faf684459e8c9f5cb7219ec
lagalapizza_1011 <52937131+SH0-ahacker@users.noreply.github.com>
2019-12-22 17:12:53 -0300

    Create .js

---
+++ scripts/open_("meme name")_MEME ()/open_bruh_MEME ()/.js
@@ -0,0 +1,12 @@
+function openInNewTab(href) {
+  Object.assign(document.createElement('a'), {
+    target: '_blank',
+    href,
+  }).click();
+}
+
+open_bruh_MEME ()
+
+function open_bruh_MEME () {
+openInNewTab("https://www.youtube.com/watch?v=D2_r4q2imnQ")
+}
