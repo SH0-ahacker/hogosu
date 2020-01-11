84c8e02657ddb6b9ed95990396e789984df746a6
lagalapizza_1011 <52937131+SH0-ahacker@users.noreply.github.com>
2020-01-11 20:55:56 -0300

    Create .js

---
+++ scripts/open_("meme name")_MEME ()/open_buttercup_MEME ()/.js
@@ -0,0 +1,12 @@
+function openInNewTab(href) {
+  Object.assign(document.createElement('a'), {
+    target: '_blank',
+    href,
+  }).click();
+}
+
+open_buttercup_MEME ()
+
+function open_buttercup_MEME () {
+openInNewTab("https://www.youtube.com/watch?v=eYDI8b5Nn5s")
+}
