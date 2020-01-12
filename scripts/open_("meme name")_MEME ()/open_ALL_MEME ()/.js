dbb219a67a6e6ac2a78987ba7be0cd4218a05d5c
bittur181 <54193311+bittur181@users.noreply.github.com>
2020-01-11 22:11:12 -0300

    Create .js

---
+++ scripts/open_("meme name")_MEME ()/open_ALL_MEME ()/.js
@@ -0,0 +1,64 @@
+function openInNewTab(href) {
+  Object.assign(document.createElement('a'), {
+    target: '_blank',
+    href,
+  }).click();
+}
+
+open_bruh_MEME ()
+
+function open_bruh_MEME () {
+openInNewTab("https://www.youtube.com/watch?v=D2_r4q2imnQ")
+}
+
+function openInNewTab(href) {
+  Object.assign(document.createElement('a'), {
+    target: '_blank',
+    href,
+  }).click();
+}
+
+open_buttercup_MEME ()
+
+function open_buttercup_MEME () {
+openInNewTab("https://www.youtube.com/watch?v=eYDI8b5Nn5s")
+}
+
+function openInNewTab(href) {
+  Object.assign(document.createElement('a'), {
+    target: '_blank',
+    href,
+  }).click();
+}
+
+open_eeeaaaooo_MEME ()
+
+function open_eeeaaaooo_MEME () {
+openInNewTab("https://www.youtube.com/watch?v=BzV4BkxtyYs")
+}
+
+function openInNewTab(href) {
+  Object.assign(document.createElement('a'), {
+    target: '_blank',
+    href,
+  }).click();
+}
+
+open_momgetthecamera_MEME ()
+
+function open_momgetthecamera_MEME () {
+openInNewTab("https://www.youtube.com/watch?v=8fsH1Rp_5bk")
+}
+
+function openInNewTab(href) {
+  Object.assign(document.createElement('a'), {
+    target: '_blank',
+    href,
+  }).click();
+}
+
+open_sadviolinairhorn_MEME ()
+
+function open_sadviolinairhorn_MEME () {
+openInNewTab("https://www.youtube.com/watch?v=BfyXK4KwyYA&list=PLPbYmF3GfbKhKZKdu551Lq776n9-Qsnlc&index=11")
+}
